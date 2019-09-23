919a9f4840a386eb9bfab82fa561a1a709fa869a
Gavin Vaske <gavin.vaske@gmail.com>
2019-09-23 18:36:45 -0500

    Trying to fix post request related bug

--- routes/api.v0.js
+++ routes/api.v0.js
@@ -62,45 +62,45 @@ router.get('/add/temperature/:temp', (request, response) => {
 
 // ADD one temperature to the database
 router.post('/temperature', (request, response) => {
-    let data = request.params
-    let temp = request.param('temp')
-    response.send('temp = ' + temp)
-    // let minTemperature = 10
-    // let maxTemperature = 50
-    // let statusCode = 406
-    // let message = "Error: An unknown error occurred"
+    // let data = request.params
+    let temp = Number(request.param('temp'))
+    // response.send('temp = ' + temp)
+    let minTemperature = 10
+    let maxTemperature = 50
+    let statusCode = 406
+    let message = "Error: An unknown error occurred"
 
-    // if(!request.body.temp){
-    //     message = "Error: Missing 'temp' body param"
-    // }
-    // // New Row that will be added to the DB
-    // var temperatureRecord = {
-    //     temperature : Number(data.temp),
-    //     timeStamp : + new Date()
-    // }
+    if(!temp){
+        message = "Error: Missing 'temp' body param"
+    }
+    // New Row that will be added to the DB
+    var temperatureRecord = {
+        temperature : temp,
+        timeStamp : + new Date()
+    }
     
-    // // Check if temperature is within the acceptable range
-    // if( temperatureRecord.temperature >= minTemperature && temperatureRecord.temperature <= maxTemperature ){
-    //     temperatureTable['temperatures'].push(temperatureRecord)
-    //     statusCode = 202
-    //     message = 'Success: Added ' + temperatureRecord.temperature + " to the database."
-    // } else {
-    //     statusCode = 406
-    //     message = 'Error: Temperature must be between the ranges 10 -> 50 degress celsius'
-    // }
+    // Check if temperature is within the acceptable range
+    if( temperatureRecord.temperature >= minTemperature && temperatureRecord.temperature <= maxTemperature ){
+        temperatureTable['temperatures'].push(temperatureRecord)
+        statusCode = 202
+        message = 'Success: Added ' + temperatureRecord.temperature + " to the database."
+    } else {
+        statusCode = 406
+        message = 'Error: Temperature must be between the ranges 10 -> 50 degress celsius'
+    }
 
-    // let responseMessage = 
-    // {
-    //     httpStatusCode: statusCode,
-    //     message: message
-    // }
-    // if(statusCode == 202){
-    //     fs.writeFile(temperatureTableFileName, JSON.stringify(temperatureTable, null, 2), function(){
-    //         response.send(responseMessage)
-    //     } ) 
-    // } else{
-    //     response.send(responseMessage)
-    // }
+    let responseMessage = 
+    {
+        httpStatusCode: statusCode,
+        message: message
+    }
+    if(statusCode == 202){
+        fs.writeFile(temperatureTableFileName, JSON.stringify(temperatureTable, null, 2), function(){
+            response.send(responseMessage)
+        } ) 
+    } else{
+        response.send(responseMessage)
+    }
 })
 
 module.exports = router
